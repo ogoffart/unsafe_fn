c406e6821477e2f5afa8416bee56acf2a5f16e58
Olivier Goffart <ogoffart@woboq.com>
2019-07-20 10:42:09 +0200

    support ignored args

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, ItemFn};
+use syn::{*, punctuated::Punctuated, spanned::Spanned};
 use quote::quote;
 
 #[proc_macro_attribute]
@@ -8,27 +8,50 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let ItemFn{ attrs, vis, constness, asyncness, unsafety, abi, ident, decl, block}
         = parse_macro_input!(item as ItemFn);
     assert!(unsafety.is_none());
-    let syn::FnDecl { fn_token, generics,  paren_token, inputs, variadic, output} = *decl;
+    let FnDecl { fn_token, generics,  paren_token, inputs, variadic, output} = *decl;
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-    let call = inputs.pairs().fold(quote!(), |x, y| {
-        let (it, sep) = y.into_tuple();
-        if let syn::FnArg::Captured(syn::ArgCaptured{
-            pat: syn::Pat::Ident(name), ..
-        }) = it {
-            quote!(#x #name #sep)
-        } else {
-            unimplemented!();
+    let mut main_param = Punctuated::<FnArg, Token!(,)>::new();
+    let mut sub_param = Punctuated::<FnArg, Token!(,)>::new();
+    let mut sub_args = Punctuated::<Ident, Token!(,)>::new();
+
+    for x in inputs.pairs() {
+        let (it, sep) = x.into_tuple();
+        match it {
+            FnArg::SelfRef(a) => {
+                unimplemented!();
+            }
+            FnArg::SelfValue(a) => {
+                unimplemented!();
+            }
+            FnArg::Captured(ArgCaptured{ pat, colon_token, ty }) => {
+                if let Pat::Ident(i) = pat {
+                    main_param.push(it.clone());
+                    sub_param.push(it.clone());
+                    sub_args.push(i.ident.clone());
+                } else {
+                    let name = Ident::new(&format!("unsafe_fn_arg{}", sub_args.len()), sep.span());
+                    main_param.push(parse(quote!(#name #colon_token #ty).into()).unwrap());
+                    sub_param.push(it.clone());
+                    sub_args.push(name);
+                }
+            }
+            FnArg::Inferred(a) => {
+                unimplemented!();
+            }
+            FnArg::Ignored(_) => {
+                main_param.push(it.clone());
+            }
         }
-    });
+    }
 
     let r = quote!{
         #(#attrs)* #vis #constness #asyncness unsafe #abi
-        #fn_token #ident #impl_generics (#inputs #variadic) #output #where_clause  {
-            #[inline] fn __unsafe_fn #impl_generics (#inputs #variadic) #output #where_clause {
+        #fn_token #ident #impl_generics (#main_param #variadic) #output #where_clause  {
+            #[inline] fn __unsafe_fn #impl_generics (#sub_param #variadic) #output #where_clause {
                 #block
             }
-            __unsafe_fn(#call)
+            __unsafe_fn(#sub_args)
         }
     };
     println!("{}", r);
