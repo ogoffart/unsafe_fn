b2acda2e4af63562769aee3752c4ea2fe17a8386
Olivier Goffart <ogoffart@woboq.com>
2019-07-20 11:46:27 +0200

    NOT WORKING: wrap self

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::{*, punctuated::Punctuated, spanned::Spanned};
+use syn::{*, punctuated::Punctuated, spanned::Spanned, fold::Fold};
 use quote::quote;
+
+const SELF_NAME : &'static str = "unsafe_fn_self";
+
+struct ReplaceSelf;
+impl Fold for ReplaceSelf {
+    fn fold_ident(&mut self, i: Ident) -> Ident {
+        if i == "self" { Ident::new(SELF_NAME, i.span()) } else { i }
+    }
+}
 
 #[proc_macro_attribute]
 pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -14,12 +23,17 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut main_param = Punctuated::<FnArg, Token!(,)>::new();
     let mut sub_param = Punctuated::<FnArg, Token!(,)>::new();
     let mut sub_args = Punctuated::<Ident, Token!(,)>::new();
+    let mut wrap_in_trait = false;
 
     for x in inputs.pairs() {
         let (it, sep) = x.into_tuple();
         match it {
-            FnArg::SelfRef(a) => {
-                unimplemented!();
+            FnArg::SelfRef(syn::ArgSelfRef{ and_token, lifetime, mutability, self_token}) => {
+                let name = Ident::new(SELF_NAME, self_token.span());
+                sub_param.push(parse(
+                    quote!(#name : #and_token #lifetime #mutability Self).into()).unwrap());
+                main_param.push(it.clone());
+                sub_args.push(self_token.clone().into());
             }
             FnArg::SelfValue(a) => {
                 unimplemented!();
@@ -45,6 +59,8 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     }
 
+    let block = fold::fold_block(&mut ReplaceSelf, *block);
+
     let r = quote!{
         #(#attrs)* #vis #constness #asyncness unsafe #abi
         #fn_token #ident #impl_generics (#main_param #variadic) #output #where_clause  {
@@ -54,7 +70,7 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
             __unsafe_fn(#sub_args)
         }
     };
-    println!("{}", r);
+    println!("-> {}", r);
     r.into()
 }
 
--- tests/test.rs
+++ tests/test.rs
@@ -2,6 +2,11 @@
 
 use unsafe_fn::unsafe_fn;
 
+#[derive(Default)]
+struct SomeStruct {
+    i: u32,
+    s: String,
+}
 
 #[unsafe_fn]
 fn hello(x : u32, foo: String) -> u32 {
@@ -15,7 +20,31 @@ fn plus_one(val : u32, _x : String) -> u32 {
     y + val + 1
 }
 
+#[unsafe_fn]
+fn take_struct(SomeStruct { i, s } : SomeStruct, foo@SomeStruct { .. } : &mut SomeStruct) -> SomeStruct {
+    let y : u32 = unsafe { std::mem::zeroed() };
+    foo.i += 1;
+    SomeStruct {
+        i: i + foo.i + y,
+        s: s + &foo.s,
+    }
+}
+
+impl SomeStruct {
+    #[unsafe_fn]
+    fn i_plus(&self, plus : u32) -> u32 {
+        let y : u32 = unsafe { std::mem::zeroed() };
+        self.i + plus + y
+    }
+}
+
 fn main() {
     assert_eq!(unsafe{hello(42, "XYZ".into())}, 42 + 3);
     assert_eq!(unsafe{plus_one(42, "XYZ".into())}, 42 + 1);
+    let mut s1 = SomeStruct{ i: 8, s: "DEF".into() };
+    let s2 = unsafe { take_struct(SomeStruct{ i: 5, s: "ABC".into() }, &mut s1 ) };
+    assert_eq!(s1.i, 9);
+    assert_eq!(s2.i, 5+9);
+    assert_eq!(s2.s, "ABCDEF");
+    assert_eq!(unsafe { s2.i_plus(58) }, 5+9+58);
 }
