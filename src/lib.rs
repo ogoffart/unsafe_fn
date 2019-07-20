a93dc007ed6c835e48514a8b1d96c1a6004db432
Olivier Goffart <ogoffart@woboq.com>
2019-07-20 14:12:44 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::{*, punctuated::Punctuated, spanned::Spanned};
 use quote::quote;
+use syn::{punctuated::Punctuated, spanned::Spanned, *};
 
 #[proc_macro_attribute]
 pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let ItemFn{ attrs, vis, constness, asyncness, unsafety, abi, ident, decl, block}
-        = parse_macro_input!(item as ItemFn);
+    let ItemFn {
+        attrs,
+        vis,
+        constness,
+        asyncness,
+        unsafety,
+        abi,
+        ident,
+        decl,
+        block,
+    } = parse_macro_input!(item as ItemFn);
     assert!(unsafety.is_none());
-    let FnDecl { fn_token, generics, paren_token : _, inputs, variadic, output} = *decl;
+    let FnDecl {
+        fn_token,
+        generics,
+        paren_token: _,
+        inputs,
+        variadic,
+        output,
+    } = *decl;
     let (impl_generics, _, where_clause) = generics.split_for_impl();
 
     let mut main_param = Punctuated::<FnArg, Token!(,)>::new();
@@ -19,19 +35,23 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
     for x in inputs.pairs() {
         let (it, sep) = x.into_tuple();
         match it {
-            FnArg::SelfRef(_) |
-            FnArg::SelfValue(_) => {
+            FnArg::SelfRef(_) | FnArg::SelfValue(_) => {
                 sub_param.push(it.clone());
                 main_param.push(it.clone());
                 wrap_self = true;
             }
-            FnArg::Captured(ArgCaptured{ pat, colon_token, ty }) => {
+            FnArg::Captured(ArgCaptured {
+                pat,
+                colon_token,
+                ty,
+            }) => {
                 if let Pat::Ident(i) = pat {
                     main_param.push(it.clone());
                     sub_param.push(it.clone());
                     sub_args.push(i.ident.clone());
                 } else {
-                    let name = Ident::new(&format!("__unsafe_fn_arg{}", sub_args.len()), sep.span());
+                    let name =
+                        Ident::new(&format!("__unsafe_fn_arg{}", sub_args.len()), sep.span());
                     main_param.push(parse(quote!(#name #colon_token #ty).into()).unwrap());
                     sub_param.push(it.clone());
                     sub_args.push(name);
@@ -46,9 +66,9 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     }
 
-    let unsafe_fn_name = Ident::new(&format!("__unsafe_fn_{}", ident.to_string()), ident.span() );
+    let unsafe_fn_name = Ident::new(&format!("__unsafe_fn_{}", ident.to_string()), ident.span());
 
-    let fun = quote!{
+    let fun = quote! {
         #[doc(hide)]
         #[inline]
         fn #unsafe_fn_name #impl_generics (#sub_param #variadic) #output #where_clause {
@@ -62,7 +82,7 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
         quote!( #unsafe_fn_name(#sub_args) )
     };
 
-    let r = quote!{
+    let r = quote! {
         #fun
         #(#attrs)* #vis #constness #asyncness unsafe #abi
         #fn_token #ident #impl_generics (#main_param #variadic) #output #where_clause  {
@@ -71,5 +91,3 @@ pub fn unsafe_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
     };
     r.into()
 }
-
-
--- tests/test.rs
+++ tests/test.rs
@@ -9,20 +9,23 @@ struct SomeStruct {
 }
 
 #[unsafe_fn]
-fn hello(x : u32, foo: String) -> u32 {
-    let y : u32 = unsafe { std::mem::zeroed() };
+fn hello(x: u32, foo: String) -> u32 {
+    let y: u32 = unsafe { std::mem::zeroed() };
     y + x + foo.len() as u32
 }
 
 #[unsafe_fn]
-fn plus_one(val : u32, _x : String) -> u32 {
-    let y : u32 = unsafe { std::mem::zeroed() };
+fn plus_one(val: u32, _x: String) -> u32 {
+    let y: u32 = unsafe { std::mem::zeroed() };
     y + val + 1
 }
 
 #[unsafe_fn]
-fn take_struct(SomeStruct { i, s } : SomeStruct, foo@SomeStruct { .. } : &mut SomeStruct) -> SomeStruct {
-    let y : u32 = unsafe { std::mem::zeroed() };
+fn take_struct(
+    SomeStruct { i, s }: SomeStruct,
+    foo @ SomeStruct { .. }: &mut SomeStruct,
+) -> SomeStruct {
+    let y: u32 = unsafe { std::mem::zeroed() };
     foo.i += 1;
     SomeStruct {
         i: i + foo.i + y,
@@ -32,36 +35,48 @@ fn take_struct(SomeStruct { i, s } : SomeStruct, foo@SomeStruct { .. } : &mut So
 
 impl SomeStruct {
     #[unsafe_fn]
-    fn i_plus(&self, plus : u32) -> u32 {
-        let y : u32 = unsafe { std::mem::zeroed() };
+    fn i_plus(&self, plus: u32) -> u32 {
+        let y: u32 = unsafe { std::mem::zeroed() };
         self.i + plus + y
     }
 
     #[unsafe_fn]
-    fn with_generic<'a, 'b, T: Clone>(&'a self, r : &'b T, _: u32, _: u32)  -> (&'b T, T, &'a str)
-        where (T, Self) : Default
+    fn with_generic<'a, 'b, T: Clone>(&'a self, r: &'b T, _: u32, _: u32) -> (&'b T, T, &'a str)
+    where
+        (T, Self): Default,
     {
-        let _ : u32 = unsafe { std::mem::zeroed() };
+        let _: u32 = unsafe { std::mem::zeroed() };
         (r, r.clone(), &self.s)
     }
 }
 
 #[unsafe_fn]
 fn create_vec<T>() -> Vec<T> {
-    let _ : u32 = unsafe { std::mem::zeroed() };
+    let _: u32 = unsafe { std::mem::zeroed() };
     Vec::new()
 }
 
 fn main() {
-    assert_eq!(unsafe{hello(42, "XYZ".into())}, 42 + 3);
-    assert_eq!(unsafe{plus_one(42, "XYZ".into())}, 42 + 1);
-    let mut s1 = SomeStruct{ i: 8, s: "DEF".into() };
-    let s2 = unsafe { take_struct(SomeStruct{ i: 5, s: "ABC".into() }, &mut s1 ) };
+    assert_eq!(unsafe { hello(42, "XYZ".into()) }, 42 + 3);
+    assert_eq!(unsafe { plus_one(42, "XYZ".into()) }, 42 + 1);
+    let mut s1 = SomeStruct {
+        i: 8,
+        s: "DEF".into(),
+    };
+    let s2 = unsafe {
+        take_struct(
+            SomeStruct {
+                i: 5,
+                s: "ABC".into(),
+            },
+            &mut s1,
+        )
+    };
     assert_eq!(s1.i, 9);
-    assert_eq!(s2.i, 5+9);
+    assert_eq!(s2.i, 5 + 9);
     assert_eq!(s2.s, "ABCDEF");
-    assert_eq!(unsafe { s2.i_plus(58) }, 5+9+58);
+    assert_eq!(unsafe { s2.i_plus(58) }, 5 + 9 + 58);
     let x = 31;
     assert_eq!(unsafe { s2.with_generic(&x, 5, 8) }, (&x, x, "ABCDEF"));
-    let _ = unsafe { create_vec::<u32>()  };
+    let _ = unsafe { create_vec::<u32>() };
 }
